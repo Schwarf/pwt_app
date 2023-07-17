18618266a2ed1aca2789063c4f7a0ac00eec71c2
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-17 20:12:18 +0200

    WIP

--- app/src/main/java/abs/apps/personal_workout_tracker/WorkoutScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/WorkoutScreen.kt
@@ -1,6 +1,11 @@
 package abs.apps.personal_workout_tracker
 
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,6 +13,8 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -25,6 +32,18 @@ fun ContactScreen(
             }
         },
         modifier = Modifier.padding(16.dp)
-    ){padding ->{}
-
+    ) { padding ->
+        if (state.isAddingWorkout) {
+            AddWorkoutDialog(state = state, onEvent = onEvent)
+        }
+        LazyColumn(content = {
+                item{
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState()),
+                        verticalAlignment = CenterVertically)
+                }
+        })
     }
+}
+
