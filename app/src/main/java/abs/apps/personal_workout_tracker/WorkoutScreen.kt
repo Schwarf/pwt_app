6b8505116504c353c973062af9c7f59ceaad16aa
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-18 14:56:40 +0200

    Align header.

--- app/src/main/java/abs/apps/personal_workout_tracker/WorkoutScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/WorkoutScreen.kt
@@ -1,9 +1,7 @@
 package abs.apps.personal_workout_tracker
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,16 +16,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -35,27 +32,28 @@ fun WorkoutEntryRow(workout: Workout, onEvent: (WorkoutEvent) -> Unit) {
     Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
         Text(text = workout.name, modifier = Modifier.weight(2f))
         Text(
-            text = workout.sets.toString(),
+            text = workout.sets,
             modifier = Modifier
-                .weight(1f)
-                .padding(start = 3.dp),
-            textAlign = TextAlign.End
+                .weight(1f),
+            textAlign = TextAlign.Center
         )
         Text(
-            text = workout.totalRepetitions.toString(),
+            text = workout.totalRepetitions,
             modifier = Modifier
-                .weight(1f)
-                .padding(start = 3.dp),
-            textAlign = TextAlign.End
+                .weight(1f),
+            textAlign = TextAlign.Center
         )
         Text(
-            text = workout.maxRepetitionsInSets.toString(),
+            text = workout.maxRepetitionsInSets,
+            modifier = Modifier
+                .weight(1f),
+            textAlign = TextAlign.Center
+        )
+        IconButton(
+            onClick = { onEvent(WorkoutEvent.DeleteWorkout(workout)) },
             modifier = Modifier
                 .weight(1f)
-                .padding(start = 3.dp),
-            textAlign = TextAlign.End
-        )
-        IconButton(onClick = { onEvent(WorkoutEvent.DeleteWorkout(workout)) }) {
+        ) {
             Icon(
                 imageVector = Icons.Default.Delete,
                 contentDescription = "Delete workout"
@@ -91,33 +89,33 @@ fun WorkoutScreen(
                             .horizontalScroll(rememberScrollState()),
                         verticalAlignment = CenterVertically
                     ) {
-                        Row(
-                            modifier = Modifier.padding(16.dp),
-                            verticalAlignment = CenterVertically
-                        ) {
-                            Text(
-                                text = "Workout",
-                                modifier = Modifier.weight(2f)
-                            )
-                            Text(
-                                text = "Sets", textAlign = TextAlign.End, modifier = Modifier
-                                    .weight(1f)
-                                    .padding(start = 3.dp)
-                            )
-                            Text(
-                                text = "Total reps", textAlign = TextAlign.End, modifier = Modifier
-                                    .weight(1f)
-                                    .padding(start = 3.dp)
-                            )
-                            Text(
-                                text = "Max reps in one set",
-                                textAlign = TextAlign.End,
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .padding(start = 3.dp)
+                        Text(
+                            text = "Workout",
+                            modifier = Modifier.weight(1f)
+                        )
+                        Text(
+                            text = "Sets", textAlign = TextAlign.End, modifier = Modifier
+                                .weight(1f)
+                        )
+                        Text(
+                            text = "Total reps", textAlign = TextAlign.End, modifier = Modifier
+                                .weight(1f)
+                        )
+                        Text(
+                            text = "Max reps in one set",
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .weight(1f)
+                        )
+                        IconButton(onClick = {}, modifier = Modifier
+                            .alpha(0f)
+                            .weight(1f))
+                        {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete workout"
                             )
                         }
-
                     }
                 }
                 items(state.workouts) { workout ->
