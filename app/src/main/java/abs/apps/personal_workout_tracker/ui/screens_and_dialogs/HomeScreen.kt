9aaacbc43d702418fc80327733c8f874798ef9b3
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-26 16:13:36 +0200

    Add bar at top of app.

--- app/src/main/java/abs/apps/personal_workout_tracker/ui/screens_and_dialogs/HomeScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/ui/screens_and_dialogs/HomeScreen.kt
@@ -9,21 +9,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,27 +34,69 @@ import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+
+/**
+ * App bar to display title and conditionally display the back navigation.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WorkoutTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     state: WorkoutState,
+    modifier: Modifier = Modifier,
     onEvent: (WorkoutEvent) -> Unit
 ) {
     val chosenWorkout = remember { mutableStateOf<Workout?>(null) }
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            WorkoutTopAppBar(
+                title = stringResource(R.string.app_name),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
         floatingActionButton =
         {
             FloatingActionButton(onClick = { onEvent(WorkoutEvent.ShowAddDialog) }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(id = R.string.add_workout))
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.add_workout)
+                )
             }
         },
-        modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+        //modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
     ) { padding ->
         if (state.isAddingWorkout) {
             AddWorkoutDialog(state = state, onEvent = onEvent)
@@ -74,11 +119,15 @@ fun HomeScreen(
                             modifier = Modifier.weight(1f)
                         )
                         Text(
-                            text = stringResource(id = R.string.workout_sets), textAlign = TextAlign.End, modifier = Modifier
+                            text = stringResource(id = R.string.workout_sets),
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
                                 .weight(1f)
                         )
                         Text(
-                            text = stringResource(id = R.string.workout_totalReps), textAlign = TextAlign.End, modifier = Modifier
+                            text = stringResource(id = R.string.workout_totalReps),
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
                                 .weight(1f)
                         )
                         Text(
@@ -117,7 +166,8 @@ fun HomeScreen(
                     }
                 }
             },
-            contentPadding = padding, modifier = Modifier.fillMaxSize(),
+            contentPadding = padding,
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
 
         )
