50003a095212965268a7c47c7beacced26f44ed5
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-27 20:52:32 +0200

    Register viewModel in AppViewModelProvider and start with ExistingWorkoutScreen.

--- app/src/main/java/abs/apps/personal_workout_tracker/ui/AppViewModelProvider.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/ui/AppViewModelProvider.kt
@@ -2,8 +2,11 @@ package abs.apps.personal_workout_tracker.ui
 
 import abs.apps.personal_workout_tracker.WorkoutTrackerApplication
 import abs.apps.personal_workout_tracker.ui.screens.AddWorkoutViewModel
+import abs.apps.personal_workout_tracker.ui.screens.ExistingWorkoutScreen
+import abs.apps.personal_workout_tracker.ui.screens.ExistingWorkoutViewModel
 import abs.apps.personal_workout_tracker.ui.screens.HomeViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -16,6 +19,13 @@ object AppViewModelProvider {
         }
         initializer {
             AddWorkoutViewModel(workoutTrackerApplication().container.workoutRepository)
+        }
+        initializer {
+            ExistingWorkoutViewModel(
+                this.createSavedStateHandle(),
+                workoutTrackerApplication().container.workoutRepository,
+                workoutTrackerApplication().container.performanceRepository
+            )
         }
 
     }
--- app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/ExistingWorkoutScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/ExistingWorkoutScreen.kt
@@ -1,11 +1,29 @@
 package abs.apps.personal_workout_tracker.ui.screens
 
 import abs.apps.personal_workout_tracker.R
+import abs.apps.personal_workout_tracker.ui.AppViewModelProvider
 import abs.apps.personal_workout_tracker.ui.navigation.INavigationDestination
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 
 object ExistingWorkoutDestination : INavigationDestination {
     override val route = "existing_workout"
     override val titleRes = R.string.workout_exists
     const val workoutIdArg = "workoutId"
     val routeWithArgs = "$route/{$workoutIdArg}"
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ExistingWorkoutScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: ExistingWorkoutViewModel = viewModel(factory= AppViewModelProvider.Factory)
+)
+{
+
 }
