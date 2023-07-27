e640781306c4bb4890221546535da19cc36b67c9
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-27 22:13:47 +0200

    Work on ExistingWorkoutScreen.

--- app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/ExistingWorkoutScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/ExistingWorkoutScreen.kt
@@ -3,11 +3,32 @@ package abs.apps.personal_workout_tracker.ui.screens
 import abs.apps.personal_workout_tracker.R
 import abs.apps.personal_workout_tracker.ui.AppViewModelProvider
 import abs.apps.personal_workout_tracker.ui.navigation.INavigationDestination
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
 
 object ExistingWorkoutDestination : INavigationDestination {
     override val route = "existing_workout"
@@ -22,8 +43,125 @@ fun ExistingWorkoutScreen(
     navigateToEditItem: (Int) -> Unit,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ExistingWorkoutViewModel = viewModel(factory= AppViewModelProvider.Factory)
+    viewModel: ExistingWorkoutViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val state by viewModel.existingWorkoutsState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            WorkoutTopAppBar(
+                title = stringResource(ExistingWorkoutDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+        ){innerPadding -> {innerPadding}
+    }
+}
+
+@Composable
+private fun ExistingWorkoutBody(
+    existingWorkout : ExistingWorkout,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        ExistingWorkoutDetails(existingWorkout = existingWorkout, )
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+    }
+}
+
+@Composable
+fun ExistingWorkoutDetails(
+    existingWorkout: ExistingWorkout,
+    modifier: Modifier = Modifier
 )
 {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            ExistingWorkoutRow(
+                labelResID = R.string.workout_name,
+                value = existingWorkout.workoutDetails.name,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ExistingWorkoutRow(
+                labelResID = R.string.workout_sets,
+                value = existingWorkout.workoutDetails.sets,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ExistingWorkoutRow(
+                labelResID = R.string.workout_totalReps,
+                value = existingWorkout.workoutDetails.totalRepetitions,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ExistingWorkoutRow(
+                labelResID = R.string.workout_maxRepsInSets,
+                value = existingWorkout.workoutDetails.maxRepetitionsInSet,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ExistingWorkoutRow(
+                labelResID = R.string.performance_preformedCounter,
+                value = existingWorkout.performanceDetails.performedCounter,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
 
+        }
+    }
+}
+
+@Composable
+private fun ExistingWorkoutRow(
+    @StringRes labelResID: Int, value: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = value, fontWeight = FontWeight.Bold)
+    }
 }
