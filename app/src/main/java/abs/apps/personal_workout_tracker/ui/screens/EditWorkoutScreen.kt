149f10e7ca1c19dc45f4257e853a59adc14044fb
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-28 17:06:21 +0200

    Add EditWorkoutScreen.

--- app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/EditWorkoutScreen.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/ui/screens/EditWorkoutScreen.kt
@@ -1,7 +1,17 @@
 package abs.apps.personal_workout_tracker.ui.screens
 
 import abs.apps.personal_workout_tracker.R
+import abs.apps.personal_workout_tracker.ui.AppViewModelProvider
 import abs.apps.personal_workout_tracker.ui.navigation.INavigationDestination
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 
 object EditWorkoutDestination : INavigationDestination {
     override val route = "workout_edit"
@@ -10,3 +20,35 @@ object EditWorkoutDestination : INavigationDestination {
     val routeWithArgs = "$route/{$itemIdArg}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditWorkoutScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditWorkoutViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            WorkoutTopAppBar(
+                title = stringResource(EditWorkoutDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        AddWorkoutBody(
+            validatedWorkoutUIState = viewModel.state,
+            onWorkoutValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateWorkout()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
