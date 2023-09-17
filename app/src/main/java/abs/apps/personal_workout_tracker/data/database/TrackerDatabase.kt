5eede469f3b1b79c3b35c5706ba7e7454cc0a567
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-17 15:26:26 +0200

    Make manual migration for renaming table.

--- app/src/main/java/abs/apps/personal_workout_tracker/data/database/TrackerDatabase.kt
+++ app/src/main/java/abs/apps/personal_workout_tracker/data/database/TrackerDatabase.kt
@@ -18,30 +18,22 @@ import androidx.sqlite.db.SupportSQLiteDatabase
     entities = [Workout::class, Timestamp::class, Training::class],
     version = 3,
     exportSchema = true,
-    autoMigrations = [AutoMigration(from = 1, to = 2), AutoMigration(
-        from = 2,
-        to = 3,
-        spec = TrackerDatabase.Migration2to3::class
-    )]
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
 )
 abstract class TrackerDatabase : RoomDatabase() {
     abstract val workoutDao: IWorkoutDao
     abstract val timestampDao: ITimestampDao
     abstract val trainingDao: ITrainingsDao
 
-    @RenameTable(fromTableName = "timestamps", toTableName = "workout_timestamps")
-    class Migration2to3 : AutoMigrationSpec
-
     companion object {
         @Volatile
         private var Instance: TrackerDatabase? = null
         fun getDatabase(context: Context): TrackerDatabase {
-            // if the Instance is not null, return it, otherwise create a new database instance.
-//            val migration2to3 = object : Migration(2, 3) {
-//                override fun migrate(database: SupportSQLiteDatabase) {
-//                    database.execSQL("CREATE TABLE IF NOT EXISTS training_timestamps")
-//                }
-//            }
+            val migration2to3 = object : Migration(2, 3) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    database.execSQL("ALTER TABLE timestamps RENAME TO workout_timestamps")
+                }
+            }
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, TrackerDatabase::class.java, "workout.db")
                     /**
@@ -49,7 +41,7 @@ abstract class TrackerDatabase : RoomDatabase() {
                      * permanently deletes all data from the tables in your database when it
                      * attempts to perform a migration with no defined migration path.
                      */
-//                    .addMigrations(migration2to3)
+                    .addMigrations(migration2to3)
                     .fallbackToDestructiveMigration()
                     .build()
                     .also { Instance = it }
